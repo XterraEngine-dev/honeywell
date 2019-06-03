cfb0c798f2b5274015c1e0ace6474f464fff1426
Desarrollador 6 <desarrollador6@caexlog.local>
2019-06-03 15:28:46 -0600

    SDK

---
+++ app/build/generated/source/buildConfig/debug/com/honeywell/barcodeexample/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.honeywell.barcodeexample;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.honeywell.barcodeexample";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 192000006;
+  public static final String VERSION_NAME = "1.92.00.0006";
+}
